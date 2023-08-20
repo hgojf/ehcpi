2d57d51b8b3ad75781ad42746d6b89a3b70e1fa2
Henry Ford <fordhenry2299@gmail.com>
2023-08-20 12:35:12 -0400

    big

--- src/ehcpi.c
+++ src/ehcpi.c
@@ -5,23 +5,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/epoll.h>
-#include <stdbool.h>
-#include <sys/timerfd.h>
 #include <signal.h>
 #include <assert.h>
 #include <stdlib.h>
 
 #include "input.h"
 
-static void sigint_handler(int signal, siginfo_t *siginfo, void *data);
 static void setup_signals(void);
+static void populate_poll(int efd, int fds[32], int argc, char **argv);
+static void epoll_loop(int efd, int debug_mode);
 
 int main(int argc, char **argv)
 {
 	int debug_mode = 0;
+	char *cfg = "/etc/ehcpi";
 	{
 	int ch;
-	while ( (ch = getopt(argc, argv, "d")) != EOF)
+	while ( (ch = getopt(argc, argv, "dC:")) != EOF)
 	{
 		switch (ch)
 		{
@@ -34,26 +34,48 @@ int main(int argc, char **argv)
 	}
 	}
 
-	parse_rules(stdin);
+	FILE *fp = fopen(cfg, "r");
+	if (!fp)
+		err(1, "fopen");
 
-	int efd = epoll_create(1);
+	parse_rules(fp);
+	if (fclose(fp) == -1)
+		err(1, "fclose");
+
+	int efd = epoll_create(32);
 	if (efd == -1)
 		err(1, "epoll_create");
 
 	//im not sure about the limits imposed on epoll (number of fds, etc)
 	int fds[32];
 
-	{
+	populate_poll(efd, fds, argc, argv);
 
+	setup_signals();
+	epoll_loop(efd, debug_mode);
+
+	free_rules();
+
+	for (int i = 0; i < 32 && fds[i] != -1; i++)
+	{
+		if (close(fds[i]) == -1)
+			err(1, "close");
+	}
+
+	if (close(efd) == -1)
+		err(1, "close");
+}
+
+static void populate_poll(int efd, int fds[32], int argc, char **argv)
+{
 	//maybe implement something like in acpid where you just check every input device
 	//and if they have an event you need add them
-	
 	int fdi = 0;
 	for (int i = optind; i < argc && i < 32; i++, fdi++)
 	{
 		int fd = open(argv[i], O_RDONLY | O_NONBLOCK);
 		if (fd == -1)
-			err(1, "open");
+			err(1, "open %s", argv[i]);
 		fds[fdi] = fd;
 		struct epoll_event event_hints = {
 			.events = EPOLLIN,
@@ -63,17 +85,26 @@ int main(int argc, char **argv)
 			err(1, "epollctl");
 	}
 	fds[fdi] = -1;
+}
 
-	}
+void sigint_handler(int signum)
+{
+	assert (signum == SIGINT);
+}
 
+static void setup_signals(void)
+{
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+		err(1, "signal");
+}
+
+static void epoll_loop(int efd, int debug_mode)
+{
 	//too small? too big?
 	struct epoll_event events[12];
 
-	{
-
 	//we only do this now because we want epoll_wait to return -1 on a SIGINT,
 	//rather than the program dying immediately
-	setup_signals();
 
 	int events_read;
 	while ( (events_read = epoll_wait(efd, events, 12, -1)) != -1)
@@ -96,37 +127,10 @@ int main(int argc, char **argv)
 				//maybe we should just fork and exec ourselves 
 				//so that we can close stdout and stderr
 				//(by redirecting them to /dev/null)
-				if (system(name) != 0)
+				if (system(name) == -1)
 					err(1, "system(%s) failed", name);
 			}
 		}
 	}
-	}
 
-	free_rules();
-
-	for (int i = 0; i < 32 && fds[i] != -1; i++)
-	{
-		if (close(fds[i]) == -1)
-			err(1, "close");
-	}
-
-	if (close(efd) == -1)
-		err(1, "close");
-}
-
-static void setup_signals(void)
-{
-	struct sigaction sigint = {
-		.sa_sigaction = sigint_handler,
-	};
-	
-	if (sigaction(SIGINT, &sigint, NULL) == -1)
-		err(1, "sigaction");
-
-}
-
-static void sigint_handler(int signal, siginfo_t *siginfo, void *data)
-{
-	assert(signal == SIGINT);
 }
---
+++ src/lerr.h
@@ -0,0 +1,4 @@
+void err(int ret, const char *fmt, ...);
+void errx(int ret, const char *fmt, ...);
+void warn(const char *fmt, ...);
+void warnx(const char *fmt, ...);
