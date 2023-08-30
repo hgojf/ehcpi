67081d2dd798be66967816a69880e705fd866581
Henry Ford <fordhenry2299@gmail.com>
2023-08-30 17:47:51 -0400

    a

--- src/input.h
+++
@@ -1,8 +0,0 @@
-#include <linux/input.h>
-
-#include <stdbool.h>
-
-const char *input_string(const struct input_event ev);
-void parse_rules(FILE *stream);
-void free_rules(void);
-bool ev_needed(int);
